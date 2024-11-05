27ba7037d9198ee3a0dadd570eb4959535cb2ef0
mnpiquero <mpiquero@uade.edu.ar>
2024-11-04 22:39:21 -0300

    acordeon

---
+++ js/accordion.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const accordionButtons = document.querySelectorAll(".accordion-btn");
+
+    accordionButtons.forEach(button => {
+        button.addEventListener("click", function() {
+            const content = this.nextElementSibling;
+
+            // Alternar la clase .expanded para mostrar/ocultar el contenido
+            content.classList.toggle("expanded");
+        });
+    });
+});
